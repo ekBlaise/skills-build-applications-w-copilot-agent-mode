2640d1bb3864955a0baf9dbfc0ec50fe7b11db21
Ekwoge Blaise <ekwogeblaise.1@gmail.com>
2025-09-18 14:24:31 +0000

    Add Codespace base URL configuration for absolute API links

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,8 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path, include
+
+# Codespace base URL (used for constructing absolute API links in this project)
+codespace_name = os.environ.get('CODESPACE_NAME')
+codespace_base = f"https://{codespace_name}-8000.app.github.dev" if codespace_name else None
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
